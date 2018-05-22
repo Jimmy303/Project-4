be33dbda80355d4990b0c2d54f950465eb8825c0
Jimmy Balgobiend <0955813@hr.nl>
2018-05-22 10:43:38 +0200

    j

---
+++ App1/App1/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "Login")]
+    public class Login : Activity
+    {
+        private Button mBtnSignUp;
+        private Button mBtnSignIn;
+        private Button mBtnGast;
+
+
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            
+            SetContentView(Resource.Layout.Login);
+
+
+            mBtnSignIn = FindViewById<Button>(Resource.Id.btnSignin);
+
+            mBtnSignIn.Click += (object sender, EventArgs args) =>
+            {
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignIn signInDialog = new dialog_SignIn();
+                signInDialog.Show(transaction, "dialog fragment2");
+            };
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignup);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+            };
+
+            mBtnGast = FindViewById<Button>(Resource.Id.button1);
+
+            mBtnGast.Click += (object sender, EventArgs args) =>
+            {
+                var intent = new Intent(this, typeof(Profiel));
+                StartActivity(intent);
+            };
+        }
+    
+    }
+}
---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Content;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private Button Startknop;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            FindViews();
+            HandleEvents();
+        }
+        private void FindViews()
+        {
+            Startknop = FindViewById<Button>(Resource.Id.startknop);
+        }
+
+        private void HandleEvents()
+        {
+            Startknop.Click += Startknop_Click;
+        }
+
+        private void Startknop_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Login));
+            StartActivity(intent);
+        }
+    }   
+
+}
+
